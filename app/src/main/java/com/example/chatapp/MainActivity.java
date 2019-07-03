0f3be73cdbcdd34b1083612a0bc8c6600e1d9397
Hulusi <37039143+hulusionel@users.noreply.github.com>
2019-07-03 19:00:37 +0300

    Toolbar hatası giderildi

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -39,9 +39,9 @@ public class MainActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-             //   Intent intent=new Intent(MainActivity.this,LoginActivity.class);
-               // startActivity(intent);
-                startActivity(new Intent(MainActivity.this,LoginActivity.class));
+                Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+                //startActivity(new Intent(MainActivity.this,LoginActivity.class));
                 //finish();
             }
         });
